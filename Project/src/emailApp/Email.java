2794d95f5964c59a8391c5cc3b0bf675a4ea3362
Nitesh <sainitesh321@gmail.com>
2021-03-17 22:25:55 +0530

    Added Variables

--- Project/src/emailApp/Email.java
+++ Project/src/emailApp/Email.java
@@ -1,5 +1,11 @@
 package emailApp;
 
 public class Email {
-
+   
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String alternateEmail;
+	private int mailboxCapacity;
+	private String department;
 }
