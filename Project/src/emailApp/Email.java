7da54c70aa3d6f1cc1b5e6bd87d21b7c9e5325df
Sai Nitesh <67225871+sainiteshb@users.noreply.github.com>
2021-03-18 13:15:22 +0530

    Add files via upload

--- Project/src/emailApp/Email.java
+++ Project/src/emailApp/Email.java
@@ -1,5 +1,7 @@
 package emailApp;
 
+import java.util.Scanner;
+
 public class Email {
    
 	private String firstName;
@@ -14,10 +16,31 @@ public class Email {
 	public Email(String firstName,String lastName) {
 		this.firstName = firstName;
 		this.lastName = lastName;
-		System.out.print("Email created:"+" "+this.firstName +" "+this.lastName);
+		System.out.println("Email created:"+" "+this.firstName +" "+this.lastName);
+		
+		// Calling the method for department
+		this.department = setDepartment();
+		System.out.println("Department"+":"+ this.department);
 	}
 	
 	// Asking for department
+	
+	private String setDepartment() {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter the department\n1 Sales\n2 Development\n3 Accounting\n0 none\nDepartment:");
+		int depChoice = in.nextInt();
+		if(depChoice==1) {
+			return "sales";
+		}else if(depChoice==2) {
+			return "dev";
+		}
+		else if(depChoice==3) {
+			return "acct";
+		}
+		else {
+			return "";
+		}
+	}
 	
 	// Generating a random password 
 	
