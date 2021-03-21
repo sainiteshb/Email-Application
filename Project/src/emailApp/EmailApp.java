05926b7b28e63407277515ccba725599a73b833e
Sai Nitesh <67225871+sainiteshb@users.noreply.github.com>
2021-03-21 12:47:01 +0530

    Update EmailApp.java

--- Project/src/emailApp/EmailApp.java
+++ Project/src/emailApp/EmailApp.java
@@ -4,7 +4,10 @@ public class EmailApp {
  
 	public static void main(String args[])
 	{
+		//  Passing the employee name
 		Email em1 = new Email("Sai","Nitesh");
-	    System.out.println(em1.showInfo());
+		
+		// Showing all the info
+	        System.out.println(em1.showInfo());
 	}
 }
