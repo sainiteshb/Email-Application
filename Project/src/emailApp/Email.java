4e8a5ee5f0963dcafe6262e4d2039637d5dc0201
Sai Nitesh <67225871+sainiteshb@users.noreply.github.com>
2021-03-21 11:42:36 +0530

    Add files via upload

--- Project/src/emailApp/Email.java
+++ Project/src/emailApp/Email.java
@@ -43,6 +43,18 @@ public class Email {
 	
 	// Generating a random password 
 	
+	private String randomPassword(int length) {
+		String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+		char[] password = new char[length];
+		for(int i=0;i<length ; i++)
+		{
+			int rand = (int)(Math.random()*passwordSet.length());
+			password[i] = passwordSet.charAt(rand);
+		}
+		return new String(password);
+				
+	}
+	
 	// Setting the mailbox capacity
 
 	// Setting an alternate email
